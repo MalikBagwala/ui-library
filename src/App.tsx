dfc665fbd278e1411ebae8bc44df225a5d443c54
MalikBagwala <malikeditz@gmail.com>
2020-01-03 09:05:26 +0530

    added support for themeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "./components/atoms/button/button.component";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Button />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -17,7 +18,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 };
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export default {
+  //   light:{
+  //       primary:
+  //   },
+  dark: {},
+  font: {
+    xxs: "8px",
+    xs: "10px",
+    md: "12px",
+    lg: "14px",
+    xl: "16px",
+    xxl: "18px"
+  }
+};
